92b75ebdfdc1196b03a8a47dca9e1b2859486d5e
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-10-12 02:59:40 +0300

    add python 3.5, 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
