e96a3670c76aee2120a413b13ed1dbbb6e5ce1fe
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-10-13 20:05:07 +0300

    add tests for decorator and context manager

--- tests/test_throttling.py
+++ tests/test_throttling.py
@@ -7,17 +7,49 @@ from aiothrottling import Throttle
 
 class TestThrottle:
 
+    NUM_ITERS = 10
+
     @pytest.fixture(params=['3/s', '6/s', '8/2s', '9/3s'])
-    def throttle(self, request):
-        return Throttle(request.param)
+    def rate(self, request):
+        return request.param
+
+    @pytest.fixture
+    def throttle(self, rate):
+        return Throttle(rate=rate)
 
     @pytest.mark.asyncio
-    async def test_rate(self, throttle):
+    async def test_decorator(self, throttle):
         interval = throttle.period / throttle.limit
         now = time() - interval
 
-        for i in range(5):
+        @throttle
+        async def foo():
+            pass
+
+        for i in range(self.NUM_ITERS):
+            await foo()
+            if i % throttle.limit == 0:
+                assert time() - now >= interval
+                now = time()
+
+    @pytest.mark.asyncio
+    async def test_awaitable(self, throttle):
+        interval = throttle.period / throttle.limit
+        now = time() - interval
+
+        for i in range(self.NUM_ITERS):
             await throttle
             if i % throttle.limit == 0:
                 assert time() - now >= interval
                 now = time()
+
+    @pytest.mark.asyncio
+    async def test_context(self, throttle):
+        interval = throttle.period / throttle.limit
+        now = time() - interval
+
+        for i in range(self.NUM_ITERS):
+            async with throttle:
+                if i % throttle.limit == 0:
+                    assert time() - now >= interval
+                    now = time()
