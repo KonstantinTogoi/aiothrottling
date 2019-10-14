c327cd41a55d05e4e804524143bc5f2f68f99b12
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-10-14 05:02:46 +0300

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(readme_path) as readme_file:
 
 setup(
     name='aiothrottling',
-    version='0.0.2',
+    version='0.0.3',
     author='Konstantin Togoi',
     author_email='konstantin.togoi@protonmail.com',
     url='https://github.com/KonstantinTogoi/aiothrottling',
