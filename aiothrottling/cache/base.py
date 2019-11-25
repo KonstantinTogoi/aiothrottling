c9673f4853797adacca564c72ea5d8a154db94ad
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-11-25 23:22:01 +0300

    add MemoryCache class

--- aiothrottling/cache/base.py
+++ aiothrottling/cache/base.py
@@ -4,11 +4,30 @@
 class Cache:
     """Simple cache interface."""
 
-    async def check_in(self, pk, digest):
+    async def check_in(self, *args):
         pass
+
+    async def check(self, *args):
+        pass
+
+    async def check_out(self, *args):
+        pass
+
+
+class MemoryCache(Cache):
+    """Cache that checks in smth as a couple of (key, digest)."""
+
+    __slots__ = ('memory', )
+
+    def __init__(self):
+        self.memory = {}
+
+    async def check_in(self, pk, digest):
+        self.memory[pk] = digest
 
     async def check(self, pk):
-        pass
+        return self.memory.get(pk)
 
     async def check_out(self, pk):
-        pass
+        if pk in self.memory:
+            del self.memory[pk]
