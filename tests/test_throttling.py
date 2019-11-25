a2faa94a8b014493e02cb59706281f9ec6f98ebe
Konstantin Togoi <konstantin.togoi@gmail.com>
2019-11-25 19:31:57 +0300

    add TestDistributedThrottle

--- tests/test_throttling.py
+++ tests/test_throttling.py
@@ -2,7 +2,7 @@ from time import time
 
 import pytest
 
-from aiothrottling import Throttle
+from aiothrottling import Throttle, DistributedThrottle
 
 
 class TestThrottle:
@@ -53,3 +53,41 @@ class TestThrottle:
                 if i % throttle.limit == 0:
                     assert time() - now >= interval
                     now = time()
+
+
+class TestDistributedThrottle:
+
+    @pytest.fixture(params=['3/s', '6/s', '8/2s', '9/3s'])
+    def rate(self, request):
+        return request.param
+
+    @pytest.fixture
+    @pytest.mark.asyncio
+    async def throttle(self, resources, rate):
+        return DistributedThrottle(resources, rate=rate)
+
+    @pytest.fixture
+    @pytest.mark.asyncio
+    async def shuffled_throttle(self, resources, rate):
+        return DistributedThrottle(resources, rate=rate, shuffle=True)
+
+    @pytest.mark.asyncio
+    async def test_acquire(self, throttle):
+        await self._test_acquire(throttle)
+
+    @pytest.mark.asyncio
+    async def test_acquire_with_shuffle(self, shuffled_throttle):
+        await self._test_acquire(shuffled_throttle)
+
+    @staticmethod
+    async def _test_acquire(throttle):
+        n_resources = len(throttle.resources)
+        interval = throttle.period / throttle.limit / n_resources
+
+        names = []
+        for i in range(len(throttle.resources) * 3):
+            last_time = 0
+            async with throttle.acquire() as resource:
+                current_time = time()
+                assert (current_time - last_time) > interval
+                names.append(resource.name)
